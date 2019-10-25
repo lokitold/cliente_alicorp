7065c2d6c56a43a2ee393bcb640c9415037f02d3
hansexe <hansexebio@upeu.edu.pe>
2019-10-24 19:06:13 -0500

    avance de gustos

---
+++ src/app/pages/favorito/favorito.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favorito',
+  templateUrl: './favorito.component.html',
+  styleUrls: ['./favorito.component.scss']
+})
+export class FavoritoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
