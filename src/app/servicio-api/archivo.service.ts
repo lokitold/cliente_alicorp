62965d11e56bfb73d2aca5b4e46cee9392a8b846
jeiner <tecjeiner@gmail.com>
2019-11-18 12:38:28 -0500

    ksksksksk

---
+++ src/app/servicio-api/archivo.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent, HttpErrorResponse, HttpEventType} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class ArchivoService {
+  SERVER_URL= " https://192.168.1.130:8000/adjuntar-archivos";
+  constructor(private httpClient: HttpClient) { }
+  public upload(formData) {
+    return this.httpClient.post<any>(this.SERVER_URL, formData, {
+      reportProgress: true,
+      observe: 'events'
+    });
+  }
+}
